0ef682900ca96ec327b08175772bcbe1dc725e04
Tobias Höglund <tobias.hoglund@visionael.com>
2011-11-11 15:38:35 +0100

    adding a name

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,3 +1,10 @@
-function TicCtrl() {
-	this.name = "yadda";
+function TicCtrl($cookieStore, $log) {
+	var scope = this;
+	
+	var name = scope.getStoredName() || "";
+	
+	scope.getStoredName = function() {
+		return $cookieStore.get("user_name");
+	}
 }
+TicCtrl.$inject = [ '$cookieStore', '$log' ];
